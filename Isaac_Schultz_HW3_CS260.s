c224adc4fa0f2c5a6f783584fea28f68c11bdbc3
Isaac <Isaac.Schultz@wsu.edu>
2018-10-25 23:20:57 -0700

    Changed Display funnction to have a dynamic prompt in $a2

--- Isaac_Schultz_HW3_CS260.s
+++ Isaac_Schultz_HW3_CS260.s
@@ -81,7 +81,8 @@ main:
 
 		# Preparing arguments for DisplayOutput function.
 			move $a0, $v0				# Stores whether the arrays are equal size $a0
-			move $a1, $v1				# Stores whether the arrays are equal size $a0
+			move $a1, $v1				# Stores the dot product in $a1
+			la $a2, V1AndV2Result		# Loads the address of the prompt to output in $a2
 			jal DisplayOutput			# Calls DisplayOutput function
 
 
@@ -95,7 +96,8 @@ main:
 
 		# Preparing arguments for DisplayOutput function.
 			move $a0, $v0				# Stores whether the arrays are equal size $a0
-			move $a1, $v1				# Stores whether the arrays are equal size $a0
+			move $a1, $v1				# Stores the dot product in $a1
+			la $a2, V1AndV3Result		# Loads the address of the prompt to output in $a2
 			jal DisplayOutput			# Calls DisplayOutput function
 
 
@@ -112,60 +114,6 @@ main:
 
 
 
-# Function that informs the user about a Dot Product of two vectors.
-# Arguments:	$a0		Boolean value if the vectors are the same size.
-#							0 Means vectors are not same size.
-#							1 Means vectors are the same size.
-#				$a1		Integer result of the dot product between two vectors.
-#							will be 0 if they are perpendicular
-#
-# Returns:		N/A
-# Uses  registers: $t0
-DisplayOutput:
-
-	move $t0, $a0								# Saves the boolean if the vectors are equal into $t0
-	
-	# We dont need to save $a1 because no part of the subroutine makes changes to it.
-
-	beq $t0, 1, DisplayOutputContinue		# If Vectors are the same size, branch to DisplayOutputContinue
-	
-	# If Vectors are not the same size
-		# Tells the user that the arrays are not the same size
-			la $a0, ArrayNotEqual
-			li $v0, PRINT_STR_SERV
-			syscall		
-		b EndDisplayOutput					# Ends the subroutine			
-	
-DisplayOutputContinue:
-	
-	# Print the start of the start of the result message
-		la $a0, V1AndV2Result
-		li $v0, PRINT_STR_SERV
-		syscall
-	
-	beq $a1, 0, VectorPerpendidular		# If the dot product is not 0, branch to VectorPerpendidular
-	
-	# if the vectors are not perpendicular, print that they are not perpendicular
-		la $a0, NotPerpendicular
-		li $v0, PRINT_STR_SERV
-		syscall
-
-	b EndDisplayOutput 						# Ends the subroutine
-
-VectorPerpendidular:
-
-	# if the vectors are perpendicular, print that they are perpendicular
-		la $a0, NotPerpendicular
-		li $v0, PRINT_STR_SERV
-		syscall
-
-EndDisplayOutput:
-	
-	jr $ra 				# Jump back to main program
-
-.end DisplayOutput
-
-  
 
 # Function that calculates the dot Product of two arrays.
 # Arguments:	Address of the first array.					(at $sp + 0)	or	0($sp)
@@ -210,7 +158,7 @@ Continue:
 								# 	We know both arrays are equal because we branched here from the conditional
 								#	statement checking if they were the same size.
 
-	lw $t3, 12($sp)				# Initialize the address of the second array.
+	lw $t3, 8($sp)				# Initialize the address of the second array.
 	
 	li $v1, 0					# Initialize the Dot Product Total
 	
@@ -219,7 +167,7 @@ Continue:
 Loop:
 	
 	beq $t0, $t2, End			# End condition for the loop.
-									#	When the loop counter equals the number of elements in the arrays
+								#	When the loop counter equals the number of elements in the arrays
 
 	lw $t5, 0($t1)				# Copying the dereferenced values of the element of the arrays.
 	lw $t6, 0($t3)
@@ -227,8 +175,8 @@ Loop:
 	mul $t4, $t5, $t6			# multiplies the current element of each array.
 	
 	add $v1, $v1, $t4			# Increases the total by the product of the current two elements.
-	addi $t1, $t1, 4				# Increment the first array pointer by 1 word.
-	addi $t3, $t3, 4				# Increment the second array pointer by 1 word.
+	addi $t1, $t1, 4			# Increment the first array pointer by 1 word.
+	addi $t3, $t3, 4			# Increment the second array pointer by 1 word.
 
 	addi $t0, $t0, 1			# Increment the loop counter
 	b Loop 						# Branch back to Loop:
@@ -238,3 +186,60 @@ End:
 	#addi $sp, $sp, 16			# Resetting the stack back to what it was at the beginning
 	jr $ra						# Jump back to the return address
 .end DotProduct
+
+
+
+
+# Function that informs the user about a Dot Product of two vectors.
+# Arguments:	$a0		Boolean value if the vectors are the same size.
+#							0 Means vectors are not same size.
+#							1 Means vectors are the same size.
+#				$a1		Integer result of the dot product between two vectors.
+#							will be 0 if they are perpendicular
+#				$a2		Address of the prompt to the user.
+#
+# Returns:		N/A
+# Uses  registers: $t0
+DisplayOutput:
+
+	move $t0, $a0								# Saves the boolean if the vectors are equal into $t0
+	
+	# We dont need to save $a1 because no part of the subroutine makes changes to it.
+
+	beq $t0, 1, DisplayOutputContinue		# If Vectors are the same size, branch to DisplayOutputContinue
+	
+	# If Vectors are not the same size
+		# Tells the user that the arrays are not the same size
+			la $a0, ArrayNotEqual
+			li $v0, PRINT_STR_SERV
+			syscall		
+		b EndDisplayOutput					# Ends the subroutine			
+	
+DisplayOutputContinue:
+	
+	# Print the start of the start of the result message
+		la $a0, $a2
+		li $v0, PRINT_STR_SERV
+		syscall
+	
+	beq $a1, 0, VectorPerpendidular		# If the dot product is not 0, branch to VectorPerpendidular
+	
+	# if the vectors are not perpendicular, print that they are not perpendicular
+		la $a0, NotPerpendicular
+		li $v0, PRINT_STR_SERV
+		syscall
+
+	b EndDisplayOutput 						# Ends the subroutine
+
+VectorPerpendidular:
+
+	# if the vectors are perpendicular, print that they are perpendicular
+		la $a0, NotPerpendicular
+		li $v0, PRINT_STR_SERV
+		syscall
+
+EndDisplayOutput:
+	
+	jr $ra 				# Jump back to main program
+
+.end DisplayOutput
