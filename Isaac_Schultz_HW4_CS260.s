e48b6b4e119458fc6f343c75435f0d36a7db9feb
Isaac <isaac.schultz@wsu.edu>
2018-11-13 13:16:18 -0800

    Added isTriangle() function

--- Isaac_Schultz_HW4_CS260.s
+++ Isaac_Schultz_HW4_CS260.s
@@ -13,6 +13,7 @@ TERMINATE_SERV  = 10
 PRINT_CHAR_SERV = 11
 
         .data  
+Two:							.float 2.0
 Triangle:						.float 2.0, 6.0, 2.0
 NotTrianglePrompt:				.asciiz "Not a triangle.\n"
 TriangleAreaPrompt:				.asciiz "Triangle area: "
@@ -24,7 +25,7 @@ main:
 	l.s $f1, 4($t0)
 	l.s $f2, 8($t0)
 	
-	# Preparing the stack for the calls
+	# Preparing the stack
 
 	addi $sp, $sp, -12					# Allocating 12 bytes on the stack
 
@@ -34,7 +35,23 @@ main:
 
 	jal semiPerimeter					# Calls the semiPerimeter function
 										#	$f0 is now the semiPerimeter.
-	addi $sp, $sp, 12					# Pops 12 bytes off the stack
+										
+	addi $sp, $sp, -4					# Allocating 4 more bytes on the stack
+	
+	la $t0, Triangle
+	l.s $f1, 0($t0)
+	l.s $f2, 4($t0)
+	l.s $f3, 8($t0)
+	
+	# Preparing the stack
+
+		s.s $f1, 0($sp)
+		s.s $f2, 4($sp)
+		s.s $f3, 8($sp)
+		s.s $f0, 12($sp)
+		
+	jal isTriangle						# Calls the isTriangle function
+										#	$v0 is now a bool representing the triangle.
 	
 	# End program
 		li $v0, TERMINATE_SERV
@@ -45,7 +62,7 @@ main:
 
 
 
-# Function that calculates the dot Product of two arrays.
+# Function that Semiperimiter of a triangle.
 # Arguments:	Length of side a					(at $sp + 0)	or	0($sp)
 #				Length of side b					(at $sp + 4)	or	4($sp)
 #				Length of side c					(at $sp + 8)	or	8($sp)
@@ -56,68 +73,105 @@ main:
 # Uses  registers: $f0, $f1
 semiPerimeter:
 	
-	l.s $f0, 0($sp)				
+	l.s $f0, 0($sp)				# loads first 2 sidelengths from the stack
 	l.s $f1, 4($sp)
-	add.s $f0, $f0, $f1
-	l.s $f1 , 8($sp)
-	add.s $f0, $f0, $f1
-		
+	add.s $f0, $f0, $f1			# adds them and stors in $f0
+	l.s $f1 , 8($sp)			# Loads the last sidelength from the stack
+	add.s $f0, $f0, $f1			# Adds the last sidelength to the other two
+	l.s $f1, Two				# Loads 2 as a float into $f1
+	div.s $f0, $f0, $f1
 	jr $ra						# Jump back to the return address
-.end DotProduct
+	
+.end semiPerimeter
 
 
 
 
-# Function that informs the user about a Dot Product of two vectors.
-# Arguments:	$a0		Boolean value if the vectors are the same size.
-#							0 Means vectors are not same size.
-#							1 Means vectors are the same size.
-#				$a1		Integer result of the dot product between two vectors.
-#							will be 0 if they are perpendicular
-#				$a2		Address of the prompt to the user.
+# Function that determines whether 3 sides can form a triangle.
+# Arguments:	Length of side a					(at $sp + 0)	or	0($sp)
+#				Length of side b					(at $sp + 4)	or	4($sp)
+#				Length of side c					(at $sp + 8)	or	8($sp)
+#				SemiPerimiter						(at $ap + 12)	or	12($sp)
 #
-# Returns:		N/A
-# Uses  registers: $t0
-DisplayOutput:
-
-	move $t0, $a0								# Saves the boolean of if the vectors are equal to $t0
+# Returns:		bool if the sidelengths are a triangle in $v0
+#					returns 0 if it is not a triangle.
+#					returns 1 if it is a triangle.
+#
+# Uses  registers: $f0, $f1, $t0
+isTriangle:
 	
-	# We dont need to save $a1 or $a2 because no part of the subroutine makes changes to them.
-
-	beq $t0, 1, DisplayOutputContinue		# If Vectors are the same size, branch to DisplayOutputContinue
+	li $t0, 0					# Initializing loop counter to 0
+	move $t1, $sp					# Saves the address of the stack pointer so that it can be iterated on.
+	l.s $f0, 12($sp)			# ssaves the semiPerimeter in $f0
 	
-	# If Vectors are not the same size
-		# Tells the user that the arrays are not the same size
-			la $a0, ArrayNotEqual
-			li $v0, PRINT_STR_SERV
-			syscall		
-		b EndDisplayOutput					# Ends the subroutine			
+Loop:
 	
-DisplayOutputContinue:
+	beq $t0, 3, WasATriangle			# End condition for the loop. When the loop counter reaches 3
+										#	Should only happen if all the sides were less than the semiperimiter
+
+	l.s $f1, ($t1)						# Load the float on the bottom of the stack
+	c.le.s $f0, $f1			 			# End the loop if one of the sides is >= semiperimiter
+	bc1f WasNotTriangle
+				
+	addi $t1, $t1, 4					# Up the stack counter to look 
+	addi $t0, $t0, 1					# Increment the loop counter
+	b Loop 								# Branch back to Loop:
+		
+WasATriangle:
+	li $v0, 1
+	b End
 	
-	# Print the start of the start of the result message
-		move $a0, $a2
-		li $v0, PRINT_STR_SERV
-		syscall
+WasNotTriangle:
+	li $v0, 0
+	b End
 	
-	beq $a1, 0, VectorPerpendidular		# If the dot product is not 0, branch to VectorPerpendidular
+End:
+	jr $ra								# Jump back to the return address
 	
-	# if the vectors are not perpendicular, print that they are not perpendicular
-		la $a0, NotPerpendicular
-		li $v0, PRINT_STR_SERV
-		syscall
+.end isTriangle
 
-	b EndDisplayOutput 						# Ends the subroutine
 
-VectorPerpendidular:
 
-	# if the vectors are perpendicular, print that they are perpendicular
-		la $a0, Perpendicular
-		li $v0, PRINT_STR_SERV
-		syscall
 
-EndDisplayOutput:
+# Function that determines whether 3 sides can form a triangle.
+# Arguments:	Length of side a					(at $sp + 0)	or	0($sp)
+#				Length of side b					(at $sp + 4)	or	4($sp)
+#				Length of side c					(at $sp + 8)	or	8($sp)
+#				SemiPerimiter						(at $ap + 12)	or	12($sp)
+#
+# Returns:		bool if the sidelengths are a triangle in $v0
+#					returns 0 if it is not a triangle.
+#					returns 1 if it is a triangle.
+#
+# Uses  registers: $f0, $f1, $t0
+triangleArea:
 	
-	jr $ra 				# Jump back to main program
+	li $t0, 0					# Initializing loop counter to 0
+	move $t1, $sp					# Saves the address of the stack pointer so that it can be iterated on.
+	l.s $f0, 12($sp)			# ssaves the semiPerimeter in $f0
+	
+Loop:
+	
+	beq $t0, 3, WasATriangle			# End condition for the loop. When the loop counter reaches 3
+										#	Should only happen if all the sides were less than the semiperimiter
 
-.end DisplayOutput
+	l.s $f1, ($t1)						# Load the float on the bottom of the stack
+	c.le.s $f0, $f1			 			# End the loop if one of the sides is >= semiperimiter
+	bc1f WasNotTriangle
+				
+	addi $t1, $t1, 4					# Up the stack counter to look 
+	addi $t0, $t0, 1					# Increment the loop counter
+	b Loop 								# Branch back to Loop:
+		
+WasATriangle:
+	li $v0, 1
+	b End
+	
+WasNotTriangle:
+	li $v0, 0
+	b End
+	
+End:
+	jr $ra								# Jump back to the return address
+	
+.end triangleArea
