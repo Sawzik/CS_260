e195cce6054d090baa3f81f99303ab96a64fe944
Isaac <isaac.schultz@wsu.edu>
2018-11-13 14:01:57 -0800

    Working for equlateral triangles, but not for others

--- Isaac_Schultz_HW4_CS260.s
+++ Isaac_Schultz_HW4_CS260.s
@@ -7,6 +7,7 @@ TAB = '\t'		# Tab
 
 # System services
 PRINT_INT_SERV  = 1
+PRINT_FLT = 2
 PRINT_STR_SERV  = 4
 READ_INT_SERV   = 5
 TERMINATE_SERV  = 10
@@ -14,7 +15,7 @@ PRINT_CHAR_SERV = 11
 
         .data  
 Two:							.float 2.0
-Triangle:						.float 2.0, 6.0, 2.0
+Triangle:						.float 10.0, 10.0, 10.0
 NotTrianglePrompt:				.asciiz "Not a triangle.\n"
 TriangleAreaPrompt:				.asciiz "Triangle area: "
 
@@ -45,13 +46,39 @@ main:
 	
 	# Preparing the stack
 
-		s.s $f1, 0($sp)
+		s.s $f1, 0($sp)					# Saves the sidelengths to the stack
 		s.s $f2, 4($sp)
 		s.s $f3, 8($sp)
-		s.s $f0, 12($sp)
+		s.s $f0, 12($sp)				# Saves the semiperimeter to the stack
 		
 	jal isTriangle						# Calls the isTriangle function
 										#	$v0 is now a bool representing the triangle.
+										
+	beq $v0, 0, ReturnNotTriangle		# If the sides are not capable of being a triangle.
+	
+	# else
+	
+	jal triangleArea					# Calls the triangleArea function.
+	
+	# Display traingle area message
+		la $a0, TriangleAreaPrompt
+		li $v0, PRINT_STR_SERV
+		syscall	
+		
+		mov.s $f12, $f0
+		li $v0, PRINT_FLT
+		syscall
+		
+		b EndProgram
+	
+ReturnNotTriangle:
+
+	# Display not a triangle message
+		la $a0, NotTrianglePrompt
+		li $v0, PRINT_STR_SERV
+		syscall	
+	
+EndProgram:	
 	
 	# End program
 		li $v0, TERMINATE_SERV
@@ -111,7 +138,7 @@ Loop:
 
 	l.s $f1, ($t1)						# Load the float on the bottom of the stack
 	c.le.s $f0, $f1			 			# End the loop if one of the sides is >= semiperimiter
-	bc1f WasNotTriangle
+	bc1t WasNotTriangle
 				
 	addi $t1, $t1, 4					# Up the stack counter to look 
 	addi $t0, $t0, 1					# Increment the loop counter
@@ -133,45 +160,36 @@ End:
 
 
 
-# Function that determines whether 3 sides can form a triangle.
+# Function that determines the area of a triangle using the length of each side and the semiPerimeter.
 # Arguments:	Length of side a					(at $sp + 0)	or	0($sp)
 #				Length of side b					(at $sp + 4)	or	4($sp)
 #				Length of side c					(at $sp + 8)	or	8($sp)
 #				SemiPerimiter						(at $ap + 12)	or	12($sp)
 #
-# Returns:		bool if the sidelengths are a triangle in $v0
-#					returns 0 if it is not a triangle.
-#					returns 1 if it is a triangle.
+# Returns:		Area of the triangle as a float in $f0
 #
-# Uses  registers: $f0, $f1, $t0
+# Uses  registers: $f0-$f3
 triangleArea:
 	
-	li $t0, 0					# Initializing loop counter to 0
-	move $t1, $sp					# Saves the address of the stack pointer so that it can be iterated on.
 	l.s $f0, 12($sp)			# ssaves the semiPerimeter in $f0
 	
-Loop:
+	l.s $f1, 0($sp)				# Load the float on the bottom of the stack
+	sub.s $f1, $f0, $f1			# Subtract the sidelength from the semiperimeter and save in $f1
 	
-	beq $t0, 3, WasATriangle			# End condition for the loop. When the loop counter reaches 3
-										#	Should only happen if all the sides were less than the semiperimiter
-
-	l.s $f1, ($t1)						# Load the float on the bottom of the stack
-	c.le.s $f0, $f1			 			# End the loop if one of the sides is >= semiperimiter
-	bc1f WasNotTriangle
-				
-	addi $t1, $t1, 4					# Up the stack counter to look 
-	addi $t0, $t0, 1					# Increment the loop counter
-	b Loop 								# Branch back to Loop:
+	l.s $f2, 4($sp)				# Load the next sidelength off the stack
+	sub.s $f2, $f0, $f2			# Subtract the sidelength from the semiperimeter and save in $f2
+	
+	l.s $f3, 4($sp)				# Load the last sidelength off the stack
+	sub.s $f3, $f0, $f3			# Subtract the sidelength from the semiperimeter and save in $f3	
+	
+	mul.s $f0, $f0, $f1			# $f0 = (semiperimiter) * (semiperimeter - side 1)
+	mul.s $f1, $f2, $f3			# $f1 = (semiperimeter - side 2) * (semiPerimeter - side 3)
+	
+	mul.s $f0, $f0, $f1			# $f0 = (semiperimiter) * (semiperimeter - side 1)
+								#		* (semiperimeter - side 2) * (semiPerimeter - side 3)
+								
+	sqrt.s $f0, $f0				# Takes the square root of the calculated value and stores it in $f0
 		
-WasATriangle:
-	li $v0, 1
-	b End
-	
-WasNotTriangle:
-	li $v0, 0
-	b End
-	
-End:
 	jr $ra								# Jump back to the return address
 	
 .end triangleArea
