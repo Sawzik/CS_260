0576f3884d7b13fcaffb415af2e4300f82d57107
Isaac <isaac.schultz@wsu.edu>
2018-11-15 12:07:07 -0800

    Working ArrayAverage Function

--- Isaac_Schultz_XtraCrdt_CS260.s
+++ Isaac_Schultz_XtraCrdt_CS260.s
@@ -32,7 +32,7 @@ main:
 	jal ArrayAverage					# Calls the semiPerimeter function
 										#	$f0 is now the average of the array's elements
 		
-	jal ArrayPrint						# Calls the isTriangle function
+	#jal ArrayPrint						# Calls the isTriangle function
 										#	$v0 is now a bool representing the triangle.
 										
 	
@@ -62,104 +62,18 @@ ArrayAverage:
 
 		l.s $f1, 0($a0)			# Loads a float from the array
 	
-		add $f0, $f0, $f1		# Increases the total the current float.
+		add.s $f0, $f0, $f1		# Increases the total the current float.
 		
-		add $a0, $a0, 4			# Increment the $a0 pointer by 1 word.
+		addi $a0, $a0, 4		# Increment the $a0 pointer by 1 word.
 		addi $t0, $t0, 1		# Increment the loop counter
 		b Loop 					# Branch back to Loop:
 		
 	End:
-		mtc1.s $a1, $f1			# Move the number of elements to a float register
-		cvt.w.s	$f1, $f1		# Convert the integer $a1 to a float $f1
+		mtc1 $a1, $f1			# Move the number of elements to a float register
+		cvt.s.w	$f1, $f1		# Convert the integer $a1 to a float $f1
 		div.s $f0, $f0, $f1		# Divide the total by the number of elements to get the average.
 		
 		jr $ra					# Jump back to the return address
 .end ArrayAverage
 
 
-
-
-# Function that determines whether 3 sides can form a triangle.
-# Arguments:	SemiPerimiter						(at $ap + 0)	or	0($sp)
-#				Length of side a					(at $sp + 4)	or	4($sp)
-#				Length of side b					(at $sp + 8)	or	8($sp)
-#				Length of side c					(at $sp + 12)	or	12($sp)
-#				
-#
-# Returns:		bool if the sidelengths are a triangle in $v0
-#					returns 0 if it is not a triangle.
-#					returns 1 if it is a triangle.
-#
-# Uses  registers: $f0-$f1, $t0-$t1
-isTriangle:
-	
-	li $t0, 0					# Initializing loop counter to 0
-	move $t1, $sp				# Saves the address of the stack pointer
-								#	so it can be iterated separate from $sp
-	addi $t1, 4					# set $t1 to point to first sidelength.
-	l.s $f0, 0($sp)				# saves the semiPerimeter in $f0
-	
-Loop:
-	
-	beq $t0, 3, WasATriangle	# End condition for the loop. Should only happen if 
-								#	all the sides were less than the semiperimiter.
-
-	l.s $f1, ($t1)				# Load the float on the bottom of the stack
-	c.le.s $f0, $f1		 		# End the loop if one of the sides is >= semiperimiter
-	bc1t WasNotTriangle
-				
-	addi $t1, $t1, 4			# Up the stack counter to look 
-	addi $t0, $t0, 1			# Increment the loop counter
-	b Loop 						# Branch back to Loop:
-		
-WasATriangle:
-	li $v0, 1					# Return 1 when the sides are a triangle
-	b End
-	
-WasNotTriangle:
-	li $v0, 0					# Return 0 if they arent a triangle.
-	b End
-	
-End:
-	jr $ra						# Jump back to the return address
-	
-.end isTriangle
-
-
-
-
-# Function that determines the area of a triangle using its sidelengths 
-# and the pre-calculated semiPerimeter.
-# Arguments:	SemiPerimiter						(at $ap + 0)	or	0($sp)
-#				Length of side a					(at $sp + 4)	or	4($sp)
-#				Length of side b					(at $sp + 8)	or	8($sp)
-#				Length of side c					(at $sp + 12)	or	12($sp)
-#
-# Returns:		Area of the triangle as a float in $f0
-#
-# Uses  registers: $f0-$f3
-triangleArea:
-	
-	l.s $f0, 0($sp)			# saves the semiPerimeter in $f0
-	
-	l.s $f1, 4($sp)			# Load the float on the bottom of the stack
-	sub.s $f1, $f0, $f1		# $f1 = semiperimeter - side a
-	
-	l.s $f2, 8($sp)			# Load the next sidelength off the stack
-	sub.s $f2, $f0, $f2		# $f2 = semiperimeter - side b
-	
-	l.s $f3, 12($sp)		# Load the last sidelength off the stack
-	sub.s $f3, $f0, $f3		# $f3 = semiperimeter - side c
-	
-	mul.s $f0, $f0, $f1		# $f0 = (semiperimiter) * (semiperimeter - side a)
-	mul.s $f1, $f2, $f3		# $f1 = (semiperimeter - side b) * (semiPerimeter - side c)
-	
-	mul.s $f0, $f0, $f1		# $f0 = (semiperimiter) * (semiperimeter - side a)
-							#		* (semiperimeter - side b) * (semiPerimeter - side c)
-								
-	sqrt.s $f0, $f0			# $f0 = SQRT[ (semiperimiter) * (semiperimeter - side a)
-							#	  * (semiperimeter - side b) * (semiPerimeter - side c) ]
-		
-	jr $ra					# Jump back to the return address
-	
-.end triangleArea
