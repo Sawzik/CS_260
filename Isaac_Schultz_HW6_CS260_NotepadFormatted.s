f175a18d8ce7ef9091b8f66de9300148e1796058
Isaac <isaac.schultz@wsu.edu>
2018-12-04 16:36:59 -0800

    added Notepad formatted file

--- Isaac_Schultz_HW6_CS260_NotepadFormatted.s
+++ Isaac_Schultz_HW6_CS260_NotepadFormatted.s
@@ -29,15 +29,15 @@ main:
 	syscall
 	move $a1, $v0
 	
-	bgt $a1, LISTSIZE, InvalidInput					# If user input is larger than the number of nodes.
+	bgt $a1, LISTSIZE, InvalidInput				# If user input is larger than the number of nodes.
 	blt $a1, LISTLOWESTINDEX, InvalidInput			# Or the user input is less than the lowest index
-													# 	Tell the user their input is invalid.
+								# 	Tell the user their input is invalid.
 	# If input is valid:
 
 	la $a0, head						# Setting up arguments for function call.
 	
 	jal FindNumber						# Runs FindNumber function
-										# $a2 is now to address of the string in the node we searched
+								# $a2 is now to address of the string in the node that was found
 	
 	# Write the string found in the node
 	move $a0, $a2
@@ -61,7 +61,7 @@ InvalidInput:
 	la $a0, Invalid_Number
 	li $v0, PRINT_STR_SERV
 	syscall	
-	b main								# Go back to the start of the program.
+	b main							# Go back to the start of the program.
 	
 EndProgram:	
 	
@@ -82,7 +82,7 @@ EndProgram:
 
 # Function that Prints the contents of a linkedList
 # Arguments:	$a0:	Address of dictionary head.
-# Returns:		N/A
+# Returns:	N/A
 # Uses  registers: $t0-$t1
 PrintList:
 	
@@ -106,11 +106,11 @@ PrintList:
 		
 		move $t0, $t1			# moves on to the next node.
 		
-		b Loop 					# Branch back to Loop:
+		b Loop 				# Branch back to Loop:
 		
 	End:
 	
-		jr $ra					# Jump back to the return address
+		jr $ra				# Jump back to the return address
 		
 .end PrintList
 
@@ -120,8 +120,8 @@ PrintList:
 
 # Function that Searches through a linked list for a numeral value.
 # Arguments:	$a0:	Address of dictionary head.
-#				$a1:	integer value to search through the list for.
-# Returns:		$a2:	The address of the italian word.
+#		$a1:	integer value to search through the list for.
+# Returns:	$a2:	The address of the italian word.
 # Uses  registers: $t0-$t1
 FindNumber:
 	
@@ -134,9 +134,9 @@ FindNumber:
 		
 		move $a0, $t1			# moves on to the next node.
 		
-		beq $t1, 0, FindEnd		# End condition for the loop. When the loop counter is larger than the list size.
+		beq $t1, 0, FindEnd		# End condition for the loop. When the address for the next node is 0.
 				
-		b FindLoop 				# Branch back to Loop:
+		b FindLoop 			# Branch back to FindLoop:
 		
 	Found:
 		
@@ -144,7 +144,7 @@ FindNumber:
 		
 	FindEnd:
 	
-		jr $ra					# Jump back to the return address
+		jr $ra				# Jump back to the return address
 		
 .end FindNumber
 
@@ -152,7 +152,7 @@ FindNumber:
 
         .data
 		
-Invalid_Number:		.asciiz "Invalid number. Try again.\n"
+Invalid_Number:			.asciiz "Invalid number. Try again.\n"
 Full_List:			.asciiz "\nFull List: "
 inputMsg:			.asciiz "Enter a number in the range of 0 through 10:"
 endMsg: 			.asciiz "\nDone!\n"
@@ -161,49 +161,49 @@ endMsg: 			.asciiz "\nDone!\n"
 
 head:
 node0:	.word 0
-		.word node1
-		.asciiz "zero"
+	.word node1
+	.asciiz "zero"
 
 node1:  .word 1
         .word node2
-		.asciiz "uno"
+	.asciiz "uno"
 
 node2:  .word 2
         .word node3
-		.asciiz "due"
+	.asciiz "due"
          
 node3:  .word 3
         .word node4
-		.asciiz "tre"
+	.asciiz "tre"
          
 node4:  .word 4
-		.word node5
-		.asciiz "quattro"
+	.word node5
+	.asciiz "quattro"
          
 node5:  .word 5
         .word node6
-		.asciiz "cinque"
+	.asciiz "cinque"
          
 node6:  .word 6
         .word node7
-		.asciiz "sei"
+	.asciiz "sei"
          
 node7:  .word 7
         .word node8
-		.asciiz "sette" 
+	.asciiz "sette" 
 
 node8:  .word 8
         .word node9
-		.asciiz "otto"
+	.asciiz "otto"
          
 node9:  .word 9
         .word node10
-		.asciiz "nove"
+	.asciiz "nove"
          
 node10: .word 10
-		.word nodeEND
-		.asciiz "dieci"
+	.word nodeEND
+	.asciiz "dieci"
 
-nodeEND:	.word -1
-			.word NULL
-			.asciiz "Fine"
+nodeEND:.word -1
+	.word NULL
+	.asciiz "Fine"
